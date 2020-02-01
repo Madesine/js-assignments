44f5bee221be73258aff0f9f418a6fb1fe5b9b5d
Madesine <kurila28@gmail.com>
2020-02-01 14:00:31 +0300

    Add tasks for Homework-1

---
+++ Homework-1/alert.js
@@ -0,0 +1,56 @@
+// // 2
+// alert("Я JavaScript!");
+
+// // 3
+// let admin = '';
+// let name = '';
+
+// name = "Джон";
+// admin = name;
+
+// alert(admin);
+
+// // 4
+// const olga = "Земля";
+// const OLGA = "Ольга";
+
+// // 5
+// let name = "Ilya";
+// alert( `hello ${1}` ); // hello 1
+// alert( `hello ${"name"}` ); // hello name
+// alert( `hello ${name}` ); // hello Ilya
+
+// // 6
+// console.log("" + 1 + 0); // = 10
+// console.log("" - 1 + 0); // = -1
+// console.log(true + false); // = 1
+// console.log(6 / "3"); // = 2
+// console.log("2" * "3"); // = 6
+// console.log(4 + 5 + "px"); // = 9px
+// console.log("$" + 4 + 5); // $45
+// console.log("4" - 2); // = 2
+// console.log("4px" - 2); // NaN
+// console.log(7 / 0); // Infinity
+// console.log("  -9  " + 5); //  =   -9  5
+// console.log("  -9  " - 5); // = -14
+// console.log(null + 1); // = 1
+// console.log(undefined + 1); // = NaN 
+// console.log(" \t \n" - 2); //= -2
+
+// // 7
+// let a = 1, b = 1;
+// let c = ++a; // a = 2 , c = 2;
+// let d = b++; // b = 1 , d = 1;
+
+// // 8
+// let a = 2;
+// let x = 1 + (a *= 2); // a = 4, x = 5;
+
+// // 9
+// 5 > 4 //true
+// "ананас" > "яблоко" //false
+// "2" > "12" // true
+// undefined == null // true
+// undefined === null // false
+// null == "\n0\n" //false
+// null === +"\n0\n" //false
