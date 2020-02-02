13d66ee7de22021685e7265db2b1583168887872
Madesine <kurila28@gmail.com>
2020-02-02 16:59:23 +0300

    Remove trash comments

--- Homework-3/script.js
+++ Homework-3/script.js
@@ -1,15 +1,15 @@
 `use strict`;
-// // 1
+// 1
 const user = {};
 
-// user.name = `John`;
-// user.surname = `Smith`;
+user.name = `John`;
+user.surname = `Smith`;
 
-// user.name = `Pete`;
+user.name = `Pete`;
 
-// delete user.name;
+delete user.name;
 
-// // 2
+// 2
 function isEmpty(obj) {
   for (const key in obj) {
 		return "false"
@@ -18,35 +18,35 @@ function isEmpty(obj) {
 }
 console.log(isEmpty(user));
 
-// // 3
-// const user = {
-//   name: "John"
-// };
-// // это будет работать?
-// user.name = "Pete"; // Yes
+// 3
+const user = {
+  name: "John"
+};
+// это будет работать?
+user.name = "Pete"; // Yes
 
-// // 4
-// const salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130
-// };
+// 4
+const salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
 
-// let sum = 0;
+let sum = 0;
 
-// for (const workerSalary in salaries) {
-// 	sum += salaries[workerSalary];
-// }
+for (const workerSalary in salaries) {
+	sum += salaries[workerSalary];
+}
 
-// // 5
-// const menu = {
-//   width: 200,
-//   height: 300,
-//   title: "My menu"
-// };
+// 5
+const menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
 
-// for (const property in menu) {
-// 	if(typeof menu[property] === "number") menu[property] *= 2;
-// }
+for (const property in menu) {
+	if(typeof menu[property] === "number") menu[property] *= 2;
+}
 
 
